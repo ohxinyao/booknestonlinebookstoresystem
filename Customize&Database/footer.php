8c3cb3f08b933a22e3400631d70cd8a2c52d8a26
phangyuxue-cmd <phangyuxue@graduate.utm.my>
2026-05-11 21:15:10 +0800

    Update footer.php

--- Customize&Database/footer.php
+++ Customize&Database/footer.php
@@ -9,6 +9,5 @@
         </div>
         <p>&copy; <?= date('Y') ?> BookNest Online Bookstore. All rights reserved.</p>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="/finalproject/booknestonlinebookstoresystem/Customize&Database/customize.js"></script>
     </div>
 </footer>
