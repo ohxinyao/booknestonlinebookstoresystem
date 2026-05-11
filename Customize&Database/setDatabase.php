23d54c00778d97ae400e7ebf0f776cf5b54a833e
phangyuxue-cmd <phangyuxue@graduate.utm.my>
2026-05-11 21:15:03 +0800

    Update setDatabase.php

--- Customize&Database/setDatabase.php
+++ Customize&Database/setDatabase.php
@@ -6,16 +6,9 @@ $password = '';
 
 try 
 {
-    //create connection object
-    $pdo = new PDO("mysql:host=$host; 
-                    dbname=$dbname; 
-                    charset=utf8", $username, $password);
-                    
-    //set error mode to exception (help while debugging)
+    $pdo = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } 
-
-// Show error message if connection fails
 catch(PDOException $e) 
 {
     die("Connection failed: " . $e->getMessage());
