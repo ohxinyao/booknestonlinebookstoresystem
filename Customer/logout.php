ae3d480859c1d566799aed8f512704f16a490890
phangyuxue-cmd <phangyuxue@graduate.utm.my>
2026-05-12 19:25:30 +0800

    Update logout.php

--- Customer/logout.php
+++ Customer/logout.php
@@ -1,6 +1,18 @@
 <?php
 session_start();
+$role = $_SESSION['user_role'] ?? 'customer';
 session_destroy();
-header("Location:login.php");
+
+if ($role == 'admin') {
+    header("Location: ../Admin/adminLogin.php");
+} 
+
+elseif ($role == 'staff') {
+    header("Location: ../Staff/staffLogin.php");
+} 
+
+else {
+    header("Location: login.php");
+}
 exit;
 ?>
