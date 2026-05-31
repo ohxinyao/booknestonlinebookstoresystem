706508dc39c03fc17333dd22798c86b954e4f8c1
phangyuxue-cmd <phangyuxue@graduate.utm.my>
2026-05-31 21:27:21 +0800

    Update removeWishList.php

--- Customer/removeWishList.php
+++ Customer/removeWishList.php
@@ -1,10 +1,14 @@
 <?php
 session_start();
+require_once '../Customize&Database/access.php';
+requireLogin();
+require_once '../Customize&Database/setDatabase.php';
+
 if (isset($_GET['id'])) {
-    $id = (int)$_GET['id'];
-    if (isset($_SESSION['wishlist'])) {
-        $_SESSION['wishlist'] = array_diff($_SESSION['wishlist'], [$id]);
-    }
+    $book_id = (int)$_GET['id'];
+    $userId = $_SESSION['user_id'];
+    $stmt = $pdo->prepare("DELETE FROM user_wishlist WHERE user_id = ? AND book_id = ?");
+    $stmt->execute([$userId, $book_id]);
 }
 header("Location: wishList.php");
 exit;
